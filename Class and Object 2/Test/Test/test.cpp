ceb954318aa9c260f4329aa7c74b31fa2cde1d8f
z-yi-han <2941412991@qq.com>
2025-09-26 22:02:26 +0800

    Add files via upload

--- Class and Object 2/Test/Test/test.cpp
+++ Class and Object 2/Test/Test/test.cpp
@@ -211,3 +211,145 @@
 //	cout << isValid("[(])[]") << endl;
 //	return 0;
 //}
+//#include<iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	//  error C2652: “Date”: 非法的复制构造函数: 第一个参数不应是“Date”
+//	 //Date d2(d1)
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+////void Func1(const Date& d)
+//void Func1(Date d)
+//{
+//	cout << &d << endl;
+//	d.Print();
+//}
+//int main()
+//{
+//	Date d1(2024, 7, 12);
+//	d1.Print();
+//
+//	// C++的规定，传值传参要调用拷贝构造
+//	Func1(d1);
+//	Date d2(d1);
+//	d2.Print();
+//
+//	return 0;
+//}
+#include<iostream>
+ using namespace std;
+
+ typedef int STDataType;
+ class Stack
+ {
+ public:
+	 Stack(int n = 4)
+		 {
+		 _a = (STDataType*)malloc(sizeof(STDataType) * n);
+		 if (nullptr == _a)
+			 {
+			 perror("malloc申请空间失败");
+			 return;
+			 }
+		
+			 _capacity = n;
+		 _top = 0;
+		 }
+	
+		 Stack(const Stack & st)
+		 {
+		 // 需要对_a指向资源创建同样⼤的资源再拷⻉值
+			 _a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+		 if (nullptr == _a)
+			 {
+			 perror("malloc申请空间失败!!!");
+			 return;
+			 }
+		
+			 memcpy(_a, st._a, sizeof(STDataType) * st._top);
+		
+			 _top = st._top;
+		 _capacity = st._capacity;
+		}
+	
+		 void Push(STDataType x)
+		 {
+	 if (_top == _capacity)
+			 {
+			 int newcapacity = _capacity * 2;
+			 STDataType * tmp = (STDataType*)realloc(_a, newcapacity *
+				sizeof(STDataType));
+			 if (tmp == NULL)
+				 {
+				 perror("realloc fail");
+				 return;
+				 }
+			
+				 _a = tmp;
+			 _capacity = newcapacity;
+			 }
+		
+			 _a[_top++] = x;
+		 }
+	
+		
+		 ~Stack()
+		 {
+		 cout << "~Stack()" << endl;
+		
+			 free(_a);
+		 _a = nullptr;
+		 _top = _capacity = 0;
+		 }
+	
+ private:
+	 STDataType * _a;
+	 size_t _capacity;
+	 size_t _top;
+};
+// 两个Stack实现队列
+class MyQueue
+{
+public:
+private:
+	Stack pushst;
+	Stack popst;
+};
+int main()
+{
+	Stack st1;
+	st1.Push(1);
+	st1.Push(2);
+	// Stack不显⽰实现拷⻉构造，⽤⾃动⽣成的拷⻉构造完成浅拷⻉
+	// 会导致st1和st2⾥⾯的_a指针指向同⼀块资源，析构时会析构两次，程序崩溃
+	Stack st2 = st1;
+	MyQueue mq1;
+	// MyQueue⾃动⽣成的拷⻉构造，会⾃动调⽤Stack拷⻉构造完成pushst/popst
+	// 的拷⻉，只要Stack拷⻉构造⾃⼰实现了深拷⻉，他就没问题
+	MyQueue mq2 = mq1;
+	return 0;
+}
