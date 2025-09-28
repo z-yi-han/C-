f542cba43f67c56effecc7f23c48310b203f9ba4
z-yi-han <2941412991@qq.com>
2025-09-28 15:28:36 +0800

    Add files via upload

--- Class and Object 2/Test/Test/test.cpp
+++ Class and Object 2/Test/Test/test.cpp
@@ -260,96 +260,345 @@
 //
 //	return 0;
 //}
+//#include<iostream>
+// using namespace std;
+//
+// typedef int STDataType;
+// class Stack
+// {
+// public:
+//	 Stack(int n = 4)
+//		 {
+//		 _a = (STDataType*)malloc(sizeof(STDataType) * n);
+//		 if (nullptr == _a)
+//			 {
+//			 perror("malloc申请空间失败");
+//			 return;
+//			 }
+//		
+//			 _capacity = n;
+//		 _top = 0;
+//		 }
+//	
+//		 Stack(const Stack & st)
+//		 {
+//		 // 需要对_a指向资源创建同样⼤的资源再拷⻉值
+//			 _a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
+//		 if (nullptr == _a)
+//			 {
+//			 perror("malloc申请空间失败!!!");
+//			 return;
+//			 }
+//		
+//			 memcpy(_a, st._a, sizeof(STDataType) * st._top);
+//		
+//			 _top = st._top;
+//		 _capacity = st._capacity;
+//		}
+//	
+//		 void Push(STDataType x)
+//		 {
+//	 if (_top == _capacity)
+//			 {
+//			 int newcapacity = _capacity * 2;
+//			 STDataType * tmp = (STDataType*)realloc(_a, newcapacity *
+//				sizeof(STDataType));
+//			 if (tmp == NULL)
+//				 {
+//				 perror("realloc fail");
+//				 return;
+//				 }
+//			
+//				 _a = tmp;
+//			 _capacity = newcapacity;
+//			 }
+//		
+//			 _a[_top++] = x;
+//		 }
+//	
+//		
+//		 ~Stack()
+//		 {
+//		 cout << "~Stack()" << endl;
+//		
+//			 free(_a);
+//		 _a = nullptr;
+//		 _top = _capacity = 0;
+//		 }
+//	
+// private:
+//	 STDataType * _a;
+//	 size_t _capacity;
+//	 size_t _top;
+//};
+//// 两个Stack实现队列
+//class MyQueue
+//{
+//public:
+//private:
+//	Stack pushst;
+//	Stack popst;
+//};
+//int main()
+//{
+//	Stack st1;
+//	st1.Push(1);
+//	st1.Push(2);
+//	// Stack不显⽰实现拷⻉构造，⽤⾃动⽣成的拷⻉构造完成浅拷⻉
+//	// 会导致st1和st2⾥⾯的_a指针指向同⼀块资源，析构时会析构两次，程序崩溃
+//	Stack st2 = st1;
+//	MyQueue mq1;
+//	// MyQueue⾃动⽣成的拷⻉构造，会⾃动调⽤Stack拷⻉构造完成pushst/popst
+//	// 的拷⻉，只要Stack拷⻉构造⾃⼰实现了深拷⻉，他就没问题
+//	MyQueue mq2 = mq1;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//// 编译报错：“operator +”必须⾄少有⼀个类类型的形参
+//int operator+(int x, int y)
+//{
+//	return x - y;
+//}
+//class A
+//{
+//public:
+//	void func()
+//	{
+//		cout << "A::func()" << endl;
+//	}
+//};
+//typedef void(A::* PF)(); //成员函数指针类型
+//int main()
+//{
+//	// C++规定成员函数要加&才能取到函数指针
+//	PF pf = &A::func;
+//	A obj;//定义ob类对象temp
+//	// 对象调⽤成员函数指针时，使⽤.*运算符
+//	(obj.*pf)();
+//	
+//		 return 0;
+//	
+//}
+//#include<iostream>
+// using namespace std;
+//
+// class Date
+// {
+// public:
+//	 Date(int year = 1, int month = 1, int day = 1)
+//		 {
+//		 _year = year;
+//		 _month = month;
+//		 _day = day;
+//		 }
+//	
+//		 void Print()
+//		 {
+//		 cout << _year << "-" << _month << "-" << _day << endl;
+//		 }
+//	
+//		 //private:
+//		 int _year;
+//	 int _month;
+//	 int _day;
+//	 };
+//
+// // 重载为全局的⾯临对象访问私有成员变量的问题
+// // 有⼏种⽅法可以解决：
+// // 1、成员放公有
+// // 2、Date提供getxxx函数
+// // 3、友元函数
+// // 4、重载为成员函数
+// bool operator==(const Date & d1, const Date & d2)
+// {
+//	 return d1._year == d2._year
+//		 && d1._month == d2._month
+//		 && d1._day == d2._day;
+//	 }
+// int main()
+// {
+//	 Date d1(2024, 7, 5);
+//	 Date d2(2024, 7, 6);
+//	 // 运算符重载函数可以显⽰调⽤
+//	 operator==(d1, d2);
+//	
+//		 // 编译器会转换成 operator==(d1, d2);
+//		 d1 == d2;
+//	
+//		 return 0;
+//	 }
+//#include<iostream>
+//using namespace std;
+//class Date {
+// public:
+//	 Date(int year = 1, int month = 1, int day = 1)
+//		 {
+//		 _year = year;
+//		 _month = month;
+//		 _day = day;
+//		 }
+//		 void Print()
+//		 {
+//		 cout << _year << "-" << _month << "-" << _day << endl;
+//		 }
+//		 bool operator==(const Date & d)
+//		 {
+//		 return _year == d._year
+//			 && _month == d._month
+//			 && _day == d._day;
+//		 }
+//	
+//		 Date & operator++()
+//		 {
+//		 cout << "前置++" << endl;
+//		 //...
+//			 return *this;
+//		 }
+//		 Date operator++(int)
+//		 {
+//		 Date tmp;
+//		 cout << "后置++" << endl;
+//			 //...
+//			 return tmp;
+//		 }
+// private:
+//	 int _year;
+//	 int _month;
+//	 int _day;
+//	 };
+//int main()
+//{
+//	Date d1(2024, 7, 5);
+//	Date d2(2024, 7, 6);
+//
+//	// 运算符重载函数可以显⽰调⽤
+//	d1.operator==(d2);
+//
+//	// 编译器会转换成 d1.operator==(d2);
+//	d1 == d2;
+//
+//	// 编译器会转换成 d1.operator++();
+//	++d1;
+//	// 编译器会转换成 d1.operator++(0);
+//	d1++;
+//
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+//	/*int GetYear()
+//	{
+//		return _year;
+//	}*/
+//
+//	bool operator==(Date d2)
+//	{
+//		return _year == d2._year
+//			&& _month == d2._month
+//			&& _day == d2._day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//
+//	//int _hour;
+//};
+//
+//
+////bool operator<(Date d1, Date d2)
+////{
+////
+////}
+//
+////bool operator==(Date d1, Date d2)
+////{
+////	return d1._year == d2._year
+////		&& d1._month == d2._month
+////		&& d1._day == d2._day;
+////}
+//
+//int main()
+//{
+//	Date x1(2024, 7, 10);
+//	Date x2(2024, 7, 11);
+//
+//	//operator==(x1, x2);
+//	//x1 == x2;
+//
+//	x1.operator==(x2);
+//	x1 == x2;
+//
+//	return 0;
+//}
 #include<iostream>
- using namespace std;
-
- typedef int STDataType;
- class Stack
+using namespace std;
+class Date
  {
  public:
-	 Stack(int n = 4)
+	 Date(int year = 1, int month = 1, int day = 1)
 		 {
-		 _a = (STDataType*)malloc(sizeof(STDataType) * n);
-		 if (nullptr == _a)
-			 {
-			 perror("malloc申请空间失败");
-			 return;
-			 }
-		
-			 _capacity = n;
-		 _top = 0;
+		 _year = year;
+		 _month = month;
+		 _day = day;
 		 }
-	
-		 Stack(const Stack & st)
+		 Date(const Date & d)
 		 {
-		 // 需要对_a指向资源创建同样⼤的资源再拷⻉值
-			 _a = (STDataType*)malloc(sizeof(STDataType) * st._capacity);
-		 if (nullptr == _a)
-			 {
-			 perror("malloc申请空间失败!!!");
-			 return;
-			 }
-		
-			 memcpy(_a, st._a, sizeof(STDataType) * st._top);
-		
-			 _top = st._top;
-		 _capacity = st._capacity;
-		}
-	
-		 void Push(STDataType x)
-		 {
-	 if (_top == _capacity)
-			 {
-			 int newcapacity = _capacity * 2;
-			 STDataType * tmp = (STDataType*)realloc(_a, newcapacity *
-				sizeof(STDataType));
-			 if (tmp == NULL)
-				 {
-				 perror("realloc fail");
-				 return;
-				 }
-			
-				 _a = tmp;
-			 _capacity = newcapacity;
-			 }
-		
-			 _a[_top++] = x;
+		 cout << " Date(const Date& d)" << endl;
+			 _year = d._year;
+		 _month = d._month;
+		 _day = d._day;
 		 }
-	
-		
-		 ~Stack()
+		 // 传引⽤返回减少拷⻉
+		 // d1 = d2;
+		 Date & operator=(const Date & d)
 		 {
-		 cout << "~Stack()" << endl;
-		
-			 free(_a);
-		 _a = nullptr;
-		 _top = _capacity = 0;
+		 // 不要检查⾃⼰给⾃⼰赋值的情况
+			 if (this != &d)
+			 {
+			 _year = d._year;
+			 _month = d._month;
+			 _day = d._day;
+			 }
+			 // d1 = d2表达式的返回对象应该为d1，也就是*this
+			 return *this;
 		 }
-	
+		 void Print()
+		 {
+		 cout << _year << "-" << _month << "-" << _day << endl;
+		 }
  private:
-	 STDataType * _a;
-	 size_t _capacity;
-	 size_t _top;
-};
-// 两个Stack实现队列
-class MyQueue
-{
-public:
-private:
-	Stack pushst;
-	Stack popst;
-};
-int main()
-{
-	Stack st1;
-	st1.Push(1);
-	st1.Push(2);
-	// Stack不显⽰实现拷⻉构造，⽤⾃动⽣成的拷⻉构造完成浅拷⻉
-	// 会导致st1和st2⾥⾯的_a指针指向同⼀块资源，析构时会析构两次，程序崩溃
-	Stack st2 = st1;
-	MyQueue mq1;
-	// MyQueue⾃动⽣成的拷⻉构造，会⾃动调⽤Stack拷⻉构造完成pushst/popst
-	// 的拷⻉，只要Stack拷⻉构造⾃⼰实现了深拷⻉，他就没问题
-	MyQueue mq2 = mq1;
-	return 0;
+	 int _year;
+	 int _month;
+	 int _day;
+	 };
+ int main()
+ {
+	 Date d1(2024, 7, 5);
+	 Date d2(d1);
+	 Date d3(2024, 7, 6);
+	 d1 = d3;
+		 // 需要注意这⾥是拷⻉构造，不是赋值重载
+		 // 请牢牢记住赋值重载完成两个已经存在的对象直接的拷⻉赋值
+		 // ⽽拷⻉构造⽤于⼀个对象拷⻉初始化给另⼀个要创建的对象
+		 Date d4 = d1;
+	
+		 return 0; 
 }
