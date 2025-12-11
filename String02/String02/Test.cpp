8ebc73efc5c64344aa338b6734139d48dfbbb73b
zyh <2941412991@qq.com>
2025-12-11 21:07:39 +0800

    Add files via upload

--- String02/String02/Test.cpp
+++ String02/String02/Test.cpp
@@ -1,23 +1,80 @@
 #include<iostream>
 #include<string>
-#include<list>
 #include<map>
+#include<list>
 using namespace std;
-void test_string()
+
+//class string
+//{
+//private:
+//	char _buff[16];
+//	char*  _str;
+//
+//	size_t _size;
+//	size_t _capacity;
+//};
+
+void test_string1()
 {
 	string s1;
 	string s2("hello world");
+
 	cout << s1 << s2 << endl;
+
 	s2[0] = 'x';
-	cout << s1<<s2 << endl;
-	//下标+【】
+	cout << s1 << s2 << endl;
+
+	// 1、下标 + []
 	for (size_t i = 0; i < s2.size(); i++)
 	{
 		cout << s2[i] << " ";
 	}
 	cout << endl;
-	//迭代器
-	string:: iterator it = s2.begin();
+
+	// 2、迭代器
+	//string::iterator it = s2.begin();
+	auto it = s2.begin();
+	while (it != s2.end())
+	{
+		*it += 2;
+
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+
+	cout << s2 << endl;
+
+	map<string, string> dict;
+	//map<string, string>::iterator mit = dict.begin();
+	auto mit = dict.begin();
+
+	/*list<int> lt = { 1,2,3,4,5,6,7 };
+	list<int>::iterator lit = lt.begin();
+	while (lit != lt.end())
+	{
+		cout << *lit << " ";
+		++lit;
+	}
+	cout << endl;*/
+
+	// 3、字符赋值，自动迭代，自动判断结束
+	// 底层就是迭代器
+	//for (auto ch : s2)
+	for (auto& ch : s2)
+	{
+		ch -= 2;
+		cout << ch << " ";
+	}
+	cout << endl;
+
+	cout << s2 << endl;
+}
+
+void test_string2()
+{
+	string s2("hello world");
+	string::iterator it = s2.begin();
 	while (it != s2.end())
 	{
 		*it += 2;
@@ -25,46 +82,7 @@ void test_string()
 		++it;
 	}
 	cout << endl;
-	//范围for,自动赋值，自动迭代，自动判断结束,底层是迭代器
-	//for (auto ch : s2)//ch是拷贝，相当于局部变量
-	for (auto& ch : s2)//这样才能修改s2
-	{
-		ch -= 2;
-		cout << ch << " ";
-	}
-	cout << endl;
-	cout << s2 << endl;
-}
-void test_list()
-{
-	list<int> lt = { 1,2,3,4,5,6,7 };
-	list<int>::iterator lit = lt.begin();
-	while (lit != lt.end())
-	{
-		cout << *lit << " ";
-		++lit;
-	}
-	cout << endl;
-}
-//void test_map()
-//{
-//	map<string, string> dict;
-//	map<string, string>::iterator mit = dict.begin();
-//	auto mit = dict.begin();
-//}
 
-void test_string2()
-{
-	string s1;
-	string s2("hello world");
-	string::iterator it = s2.begin();
-	while (it != s2.end())
-	{
-		cout << *it << " ";
-		++it;
-	}
-	cout << endl;
-	//反向迭代器,一定不是原生指针
 	string::reverse_iterator rit = s2.rbegin();
 	while (rit != s2.rend())
 	{
@@ -72,22 +90,21 @@ void test_string2()
 		++rit;
 	}
 	cout << endl;
-	//const迭代器
-	const string s3{ "hello world" };
-	//string::iterator it = s3.begin();报错
-	string::const_iterator cit = s3.begin();
-	while (it != s2.end())
+
+	const string s3("hello world");
+	//string::const_iterator cit = s3.begin();
+	auto cit = s3.begin();
+	while (cit != s3.end())
 	{
-		//*cit += 2;报错
-		cout << *it << " ";
-		++it;
+		//*cit += 2;
+		cout << *cit << " ";
+		++cit;
 	}
 	cout << endl;
-	//const反向迭代器
-	const string s4("hello world");
+
 	//string::const_reverse_iterator rcit = s3.rbegin();
-	auto rcit = s4.rbegin();//auto简便
-	while (rcit != s4.rend())
+	auto rcit = s3.rbegin();
+	while (rcit != s3.rend())
 	{
 		// *rcit += 2;
 		cout << *rcit << " ";
@@ -120,46 +137,110 @@ void TestPushBack()
 
 void test_string3()
 {
-	string s5{ "hello world" };
-	cout<<s5.length()<<endl;
-	cout << s5.size() << endl;
-	cout << s5.max_size() << endl;
-	cout << s5.capacity() << endl;
+	string s2("hello world");
+	cout << s2.length() << endl;
+	cout << s2.size() << endl;
+
+	cout << s2.max_size() << endl;
+
+	cout << s2.capacity() << endl;
 	TestPushBack();
 
-	string s6("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-	cout << sizeof(s5) << endl;
-	cout << sizeof(s6) << endl;
+	string s3("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	//cout << sizeof(s2) << endl;
+	//cout << sizeof(s3) << endl;
 }
+
 void test_string4()
 {
-	string s7("hello worldxxxxxxxxxxxxx");
-	cout << s7.size() << endl;
-	cout << s7.capacity() << endl << endl;
+	string s2("hello worldxxxxxxxxxxxxx");
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl << endl;
 
-	s7.reserve(20);
-	cout << s7.size() << endl;
-	cout << s7.capacity() << endl << endl;
+	s2.reserve(20);
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl << endl;
 
-	s7.reserve(28);
-	cout << s7.size() << endl;
-	cout << s7.capacity() << endl << endl;
+	s2.reserve(28);
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl << endl;
 
-	s7.reserve(40);
-	cout << s7.size() << endl;
-	cout << s7.capacity() << endl << endl;
+	s2.reserve(40);
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl << endl;
 
-	s7.clear();
-	cout << s7.size() << endl;
-	cout << s7.capacity() << endl << endl;
+	s2.clear();
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl << endl;
+
+	cout << typeid(string::iterator).name() << endl;
+	cout << typeid(string::reverse_iterator).name() << endl;
 
 }
+
 int main()
 {
-	//test_string();
-	//test_list();
-	//test_string2();
-	//test_string3();
 	test_string4();
+
 	return 0;
 }
+
+//int func1()
+//{
+//	return 10;
+//}
+//
+//// 不能做参数
+////void func0(auto a = 0)
+////{}
+//
+//auto func2()
+//{
+//	//...
+//	return func1();
+//}
+//
+//auto func3()
+//{
+//	//...
+//	return func2();
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	auto b = a;
+//	auto c = 'a';
+//	auto d = func1();
+//	// 编译报错:rror C3531: “e”: 类型包含“auto”的符号必须具有初始值设定项
+//	//auto e;
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;
+//	cout << typeid(d).name() << endl;
+//
+//	//auto array[] = { 4, 5, 6 };
+//
+//	auto ret = func3();
+//
+//	int array[] = { 1, 2, 3, 4, 5 };
+//	// C++98的遍历
+//	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+//	{
+//		array[i] *= 2;
+//	}
+//	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+//	{
+//		cout << array[i] << endl;
+//	}
+//	
+//	// yyds
+//	// 范围for适用于容器 和 数组
+//	// C++11的遍历
+//	for (auto& e : array)
+//		e *= 2;
+//
+//	for (auto e : array)
+//		cout << e << " " << endl;
+//
+//	return 0;
+//}
